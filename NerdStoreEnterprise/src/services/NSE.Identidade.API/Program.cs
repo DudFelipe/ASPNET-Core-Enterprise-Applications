eddeb4ab0c887a550d9dc28694ebc9516610996f
Eduardo Felipe <eduardo_donadio@outlook.com>
2024-03-15 14:57:05 -0300

    Commit inicial

---
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
